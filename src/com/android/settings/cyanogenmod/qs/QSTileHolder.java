LineageOS/android_packages_apps_Settings

settings: dynamic tiles
Add support for dynamic tiles

Demo: https://cloud.ruesga.com/f/71a12e4801/
Required: topic:customtiles_respkg
          topic:readd_customtiles
JIRA: CML-22

Change-Id: I284b3c50efa379a936a9f7d7087404053cced651
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -18,11 +18,11 @@ package com.android.settings.cyanogenmod.qs;
 import android.content.Context;
 
 import android.text.TextUtils;
+
+import com.android.internal.util.cm.DynamicQSUtils;
 import com.android.internal.util.cm.QSConstants;
 import com.android.internal.util.cm.QSUtils;
 import com.android.settings.R;
-
-import java.util.Arrays;
 
 /**
  * This class holds the icon, the name - or the string the user sees,
@@ -46,7 +46,8 @@ public class QSTileHolder {
         int stringId = -1;
 
         if (!TILE_ADD_DELETE.equals(tileType) &&
-                !QSUtils.getAvailableTiles(context).contains(tileType)) {
+                !QSUtils.getAvailableTiles(context).contains(tileType) &&
+                !DynamicQSUtils.getAvailableTiles(context).contains(tileType)) {
             return null;
         }
 
@@ -161,6 +162,21 @@ public class QSTileHolder {
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_tile_sync;
                 break;
+
+            // Dynamic tiles
+            case QSConstants.DYNAMIC_TILE_NEXT_ALARM:
+                resourceName = "ic_dynamic_qs_next_alarm";
+                stringId = R.string.qs_dynamic_tile_next_alarm;
+                break;
+            case QSConstants.DYNAMIC_TILE_IME_SELECTOR:
+                resourceName = "ic_dynamic_qs_ime_selector";
+                stringId = R.string.qs_dynamic_tile_ime_selector;
+                break;
+            case QSConstants.DYNAMIC_TILE_BUG_REPORT:
+                resourceName = "ic_dynamic_qs_bug_report";
+                stringId = R.string.qs_dynamic_tile_bug_report;
+                break;
+
             default:
                 return null;
         }

ProjectX-Android/packages_apps_Settings

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -18,11 +18,10 @@ package com.android.settings.cyanogenmod.qs;
 import android.content.Context;
 
 import android.text.TextUtils;
+
 import com.android.internal.util.cm.QSConstants;
 import com.android.internal.util.cm.QSUtils;
 import com.android.settings.R;
-
-import java.util.Arrays;
 
 /**
  * This class holds the icon, the name - or the string the user sees,
@@ -161,6 +160,25 @@ public class QSTileHolder {
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_tile_sync;
                 break;
+
+            // Dynamic tiles
+            case QSConstants.DYNAMIC_TILE_NEXT_ALARM:
+                resourceName = "ic_dynamic_qs_next_alarm";
+                stringId = R.string.qs_dynamic_tile_next_alarm;
+                break;
+            case QSConstants.DYNAMIC_TILE_IME_SELECTOR:
+                resourceName = "ic_dynamic_qs_ime_selector";
+                stringId = R.string.qs_dynamic_tile_ime_selector;
+                break;
+            case QSConstants.DYNAMIC_TILE_SU:
+                resourceName = "ic_dynamic_qs_su";
+                stringId = R.string.qs_dynamic_tile_su;
+                break;
+            case QSConstants.DYNAMIC_TILE_ADB:
+                resourceName = "ic_dynamic_qs_adb";
+                stringId = R.string.qs_dynamic_tile_adb;
+                break;
+
             default:
                 return null;
         }